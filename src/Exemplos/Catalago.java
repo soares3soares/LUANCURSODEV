82673b0709e4018fb95f712d7b51f43f53348419
26139597 <luansoares.criacoes@hotmail.com>
2019-02-21 20:01:36 -0300

    exemplo - switch

--- src/Exemplos/Catalago.java
+++ src/Exemplos/Catalago.java
@@ -10,7 +10,31 @@ public class Catalago {
             a=b;
         }
         System.out.println(a);
+        if (c>a){
+            c=a;
+        } else {
+            c=b;
+        }
+    System.out.println(c);
+    //switch
+    int op=3;
+    int v=0;
+    switch (op) {
+        case 1:
+            v = 1;
+            break;
+        case 2:
+            v = 2;
+            break;
+        default:
+            v = 1;
     }
+     System.out.println(v);
+
+    }
+
+    }
+
 
 
 }
